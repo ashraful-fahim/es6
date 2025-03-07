ffd8755e77912909cee155757bb974eb3f232a5c
ashraful-fahim <ashraful30june@gmail.com>
2025-03-07 23:28:41 +0600

    practiced more closure

--- closure.js
+++ closure.js
@@ -1,12 +1,28 @@
 /* A closure is a function that has access to its own scope, the outer function’s variables, and global variables, even after the outer function has finished executing. This enables functions to “remember” their environment.*/
 
-function outer(){
+function outer() {
     let outerVar = "I'm in the outer scope!";
-    function inner(){
+    function inner() {
         console.log(outerVar);
     }
-    // return inner;
+    return inner;
 }
 
 const closure = outer();
 closure();
+
+
+// example - 2 
+function sum() {
+    let counter = 0;
+    return function () {
+        counter++;
+        console.log(counter)
+    }
+}
+
+const result = sum();
+result();
+result();
+result();
+result();
